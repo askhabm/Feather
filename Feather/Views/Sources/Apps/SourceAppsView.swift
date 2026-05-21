0167cc21f517368f7ac82f4d88066362e54f93a7
askhabm <99859257+askhabm@users.noreply.github.com>
2026-05-21 15:58:16 +0300

    Update SourceAppsView.swift

--- Feather/Views/Sources/Apps/SourceAppsView.swift
+++ Feather/Views/Sources/Apps/SourceAppsView.swift
@@ -1,97 +1,203 @@
 //
-//  SourcesView.swift
+//  SourceAppsView.swift
 //  Feather
 //
-//  Created by samara on 10.04.2025.
+//  Created by samara on 1.05.2025.
 //
 
-import CoreData
-import AltSourceKit
 import SwiftUI
+import AltSourceKit
 import NimbleViews
+import UIKit
 
-// MARK: - View
-struct SourcesView: View {
-	@StateObject var viewModel = SourcesViewModel.shared
-	@State private var _selectedRoute: SourceAppsView.SourceAppRoute?
-	@State private var _searchText = ""
-	@AppStorage("Feather.sortOptionRawValue") private var _sortOptionRawValue: String = SourceAppsView.SortOption.default.rawValue
-	@AppStorage("Feather.sortAscending") private var _sortAscending: Bool = true
-	@State private var _sortOption: SourceAppsView.SortOption = .default
-
-	@FetchRequest(
-		entity: AltSource.entity(),
-		sortDescriptors: [NSSortDescriptor(keyPath: \AltSource.name, ascending: true)],
-		animation: .snappy
-	) private var _sources: FetchedResults<AltSource>
-
-	var body: some View {
-		NBNavigationView("Каталог") {
-			ZStack {
-				if !viewModel.isFinished {
-					ProgressView()
-				} else {
-					let loadedSources = Array(_sources).compactMap { viewModel.sources[$0] }
-					if loadedSources.isEmpty {
-						if #available(iOS 17, *) {
-							ContentUnavailableView {
-								Label("Нет приложений", systemImage: "globe.desk.fill")
-							} description: {
-								Text("Репозиторий загружается...")
-							}
-						}
-					} else {
-						SourceAppsTableRepresentableView(
-							sources: loadedSources,
-							searchText: $_searchText,
-							sortOption: $_sortOption,
-							sortAscending: $_sortAscending,
-							onSelect: { _selectedRoute = $0 }
-						)
-						.ignoresSafeArea()
-					}
-				}
-			}
-			.searchable(text: $_searchText, placement: .platform())
-			.toolbar {
-				NBToolbarMenu(
-					systemImage: "line.3.horizontal.decrease",
-					style: .icon,
-					placement: .topBarTrailing
-				) {
-					Section("Сортировка") {
-						ForEach(SourceAppsView.SortOption.allCases, id: \.displayName) { opt in
-							Button {
-								if _sortOption == opt {
-									_sortAscending.toggle()
-								} else {
-									_sortOption = opt
-									_sortAscending = true
-								}
-							} label: {
-								HStack {
-									Text(opt.displayName)
-									if _sortOption == opt {
-										Image(systemName: _sortAscending ? "chevron.up" : "chevron.down")
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			.navigationDestinationIfAvailable(item: $_selectedRoute) { route in
-				SourceAppsDetailView(source: route.source, app: route.app)
-			}
-			.refreshable {
-				await viewModel.fetchSources(_sources, refresh: true)
+// MARK: - Extension: View (Enil)
+extension SourceAppsView {
+	enum SortOption: String, CaseIterable {
+		case `default` = "default"
+		case name
+		case date
+		
+		var displayName: String {
+			switch self {
+			case .default:  .localized("Default")
+			case .name: 	.localized("Name")
+			case .date: 	.localized("Date")
 			}
 		}
-		.task(id: Array(_sources)) {
-			await viewModel.fetchSources(_sources)
+	}
+}
+
+// MARK: - View
+struct SourceAppsView: View {
+	@AppStorage("Feather.sortOptionRawValue") private var _sortOptionRawValue: String = SortOption.default.rawValue
+	@AppStorage("Feather.sortAscending") private var _sortAscending: Bool = true
+	
+	@State private var _sortOption: SortOption = .default
+	@State private var _selectedRoute: SourceAppRoute?
+	
+	@State var isLoading = true
+	@State var hasLoadedOnce = false
+	@State private var _searchText = ""
+
+	private var _navigationTitle: String {
+		if object.count == 1 {
+			object[0].name ?? .localized("Unknown")
+		} else {
+			.localized("%lld Sources", arguments: object.count)
+		}
+	}
+	
+	var object: [AltSource]
+	@ObservedObject var viewModel: SourcesViewModel
+	@State private var _sources: [ASRepository]?
+	
+	// MARK: Body
+	var body: some View {
+		ZStack {
+			if
+				let _sources,
+				!_sources.isEmpty
+			{
+				SourceAppsTableRepresentableView(
+					sources: _sources,
+					searchText: $_searchText,
+					sortOption: $_sortOption,
+					sortAscending: $_sortAscending,
+					onSelect: {self._selectedRoute = $0}
+				)
+				.ignoresSafeArea()
+			} else {
+				ProgressView()
+			}
+		}
+		.navigationTitle(_navigationTitle)
+		.searchable(text: $_searchText, placement: .platform())
+		.toolbarTitleMenu {
+			if
+				let _sources,
+				_sources.count == 1
+			{
+				if let url = _sources[0].website {
+					Button(.localized("Visit Website"), systemImage: "globe") {
+						UIApplication.open(url)
+					}
+				}
+				
+				if let url = _sources[0].patreonURL {
+					Button(.localized("Visit Patreon"), systemImage: "dollarsign.circle") {
+						UIApplication.open(url)
+					}
+				}
+			}
+			
+			Divider()
+			
+			Button(.localized("Copy"), systemImage: "doc.on.doc") {
+				guard !object.isEmpty else {
+					UIAlertController.showAlertWithOk(
+						title: .localized("Error"),
+						message: .localized("No sources to copy")
+					)
+					return
+				}
+				UIPasteboard.general.string = object.map {
+					$0.sourceURL!.absoluteString
+				}.joined(separator: "\n")
+				UIAlertController.showAlertWithOk(
+					title: .localized("Success"),
+					message: .localized("Sources copied to clipboard")
+				)
+			}
+		}
+		.toolbar {
+			NBToolbarMenu(
+				systemImage: "line.3.horizontal.decrease",
+				style: .icon,
+				placement: .topBarTrailing
+			) {
+				_sortActions()
+			}
+		}
+		.onAppear {
+			if !hasLoadedOnce, viewModel.isFinished {
+				_load()
+				hasLoadedOnce = true
+			}
+			_sortOption = SortOption(rawValue: _sortOptionRawValue) ?? .default
+		}
+		.onChange(of: viewModel.isFinished) { _ in
+			_load()
 		}
 		.onChange(of: _sortOption) { newValue in
 			_sortOptionRawValue = newValue.rawValue
+		}
+		.navigationDestinationIfAvailable(item: $_selectedRoute) { route in
+			SourceAppsDetailView(source: route.source, app: route.app)
+		}
+	}
+	
+	private func _load() {
+		isLoading = true
+		
+		Task {
+			let loadedSources = object.compactMap { viewModel.sources[$0] }
+			_sources = loadedSources
+			withAnimation(.easeIn(duration: 0.2)) {
+				isLoading = false
+			}
+		}
+	}
+	
+	struct SourceAppRoute: Identifiable, Hashable {
+		let source: ASRepository
+		let app: ASRepository.App
+		let id: String = UUID().uuidString
+	}
+}
+
+// MARK: - Extension: View (Sort)
+extension SourceAppsView {
+	@ViewBuilder
+	private func _sortActions() -> some View {
+		Section(.localized("Filter by")) {
+			ForEach(SortOption.allCases, id: \.displayName) { opt in
+				_sortButton(for: opt)
+			}
+		}
+	}
+	
+	private func _sortButton(for option: SortOption) -> some View {
+		Button {
+			if _sortOption == option {
+				_sortAscending.toggle()
+			} else {
+				_sortOption = option
+				_sortAscending = true
+			}
+		} label: {
+			HStack {
+				Text(option.displayName)
+				Spacer()
+				if _sortOption == option {
+					Image(systemName: _sortAscending ? "chevron.up" : "chevron.down")
+				}
+			}
+		}
+	}
+}
+
+import SwiftUI
+
+extension View {
+	@ViewBuilder
+	func navigationDestinationIfAvailable<Item: Identifiable & Hashable, Destination: View>(
+		item: Binding<Item?>,
+		@ViewBuilder destination: @escaping (Item) -> Destination
+	) -> some View {
+		if #available(iOS 17, *) {
+			self.navigationDestination(item: item, destination: destination)
+		} else {
+			self
 		}
 	}
 }
